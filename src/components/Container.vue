5662a43af045827db271cb89ab7a0af7f3bfa37f
Kuzey Kurtuluş <kuzeeeyk@gmail.com>
2022-09-09 15:08:18 +0300

    Fixed SEO & accessibility issues

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex pt-20 md:flex-row flex-col-reverse gap-4 items-center">
+  <div class="flex pt-20 md:flex-row flex-col-reverse gap-16 items-center">
     <div class="flex flex-col justify-center grow">
       <h1
-        class="font-bold dark:text-white text-black md:text-start text-center"
+        class="font-bold text-lg dark:text-white text-black md:text-start text-center"
       >
         About me
       </h1>
-      <p class="dark:text-white text-black md:text-start text-center">
+      <p class="dark:text-white text-black md:text-start text-center opacity-60">
         Hey, I'm a 17 year old developer from Turkey, I'm currently focused on
         my exams but I'm still trying to learn new things and improve my skills.
         I'm a backend developer mainly experienced in Node.js, but I'm also
@@ -17,8 +17,8 @@
     <div class="w-36 flex-shrink-0 relative">
       <img
         class="rounded-[4.5rem] w-36 h-36 md:hover:rounded-[1rem] duration-150 cursor-pointer"
-        src="https://cdn.discordapp.com/attachments/990340026573398056/1017524246714527824/E57C6DDF-9570-4A41-B981-28C885139121.png"
-        alt=""
+                src="https://cdn.discordapp.com/attachments/990340026573398056/1017758203062255667/IMG_4546.AVIF"
+        alt="Profile picture"
       />
       <div
         class="mobile-status md:hidden w-6 h-6 rounded-full bg-green-400 absolute bottom-2 right-2 border-4 border-black"
--- src/components/RepositoriesRedirect.vue
+++ src/components/RepositoriesRedirect.vue
@@ -6,7 +6,7 @@
     <div class="md:grid md:grid-cols-2 md:grid-rows-1 gap-4 mt-4">
       <router-link to="/repos">
         <div
-          class="flex flex-row items-center justify-center p-4 transition-colors hover:bg-white/10 rounded-lg"
+          class="flex flex-row items-center justify-center p-4 transition-colors bg-white/5 hover:bg-white/10 rounded-lg"
         >
           <p class="text-neutral-500 ml-8">My repositories</p>
           <img
@@ -14,12 +14,13 @@
             width="24"
             height="24"
             class="ml-5"
+            alt="My repositories"
           />
         </div>
       </router-link>
       <router-link event="" to="blog" class="pointer-events-none">
         <div
-          class="flex flex-row items-center justify-center p-4 transition-colors hover:bg-white/10 rounded-lg"
+          class="flex flex-row items-center justify-center p-4 transition-colors bg-white/5 hover:bg-white/10 rounded-lg"
         >
           <p class="text-neutral-500 ml-8">Blog (coming soon...)</p>
           <img
@@ -27,6 +28,7 @@
             width="24"
             height="24"
             class="ml-5"
+            alt="Blog (coming soon...)"
           />
         </div>
       </router-link>

7fe96d7ccae09c404ee39502d8847a29101db1cb
Snowy <63533083+1000snowy@users.noreply.github.com>
2022-09-09 14:02:47 +0300

    Resize container

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="md:w-[600px] w-[75%] mx-auto md:text-black">
+  <div class="md:w-[600px] sm:w-[75%] w-[90%] mx-auto md:text-black">
     <Header />
     <router-view></router-view>
   </div>

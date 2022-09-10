accde5e6d042c41a9fa98592e2a56ecdbcc3aad7
Snowy <63533083+1000snowy@users.noreply.github.com>
2022-09-10 15:19:27 +0300

    Update router.ts

--- src/router.ts
+++ src/router.ts
@@ -3,10 +3,12 @@ import { createRouter, createWebHistory } from "vue-router";
 import Landing from "./views/Landing.vue";
 import Repositories from "./views/Repositories.vue";
 import Contact from "./views/Contact.vue";
+import ErrorPage from "./views/404Page.vue";
 
 export default createRouter({
   history: createWebHistory(),
   routes: [
+    { path: "/:pathMatch(.*)*", component: ErrorPage },
     {
       path: "/",
       component: Landing,

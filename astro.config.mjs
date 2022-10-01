b7f4581d5d05fa16757431d95c0fc7d22593d490
Snowy <63533083+1000snowy@users.noreply.github.com>
2022-10-01 21:21:37 +0300

    Progresses in dev branch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
+
+import image from "@astrojs/image";
 
 export default defineConfig({
-  integrations: [
-    mdx(),
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-  ],
+  integrations: [tailwind(), sitemap(), prefetch(), image()],
 });
